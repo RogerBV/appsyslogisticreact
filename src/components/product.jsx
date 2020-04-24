267a3a1210348a0959ec93656ab94e132fe9f43a
RogerBV <rogerbv_9017@hotmail.com>
2020-04-24 13:16:06 -0500

    Implement Call Child Component method

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -9,75 +9,104 @@ class Product extends Component {
             ,price:0
             ,categoryId:0
             ,categories:[]
-            ,ip:'192.168.1.61'
+            ,ip:'192.168.0.25'
         }
-        this.handleChangeCategory = this.handleChangeCategory.bind(this);
-        this.handleChangeName = this.handleChangeName.bind(this);
-        this.handleChangeDescription = this.handleChangeDescription.bind(this);
-        this.handleChangePrice = this.handleChangePrice.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    
         this.SaveProduct = this.SaveProduct.bind(this);
     }
-    handleChangeName(event){
-        this.setState({ name:event.target.value });
-    }
-    handleChangeDescription(event){
-        this.setState({ description: event.target.value })
-    }
-    handleChangePrice(event){
-        this.setState({ price: event.target.value });
-    }
-    handleChangeCategory(event){
-        this.setState({ categoryId:event.target.value });
+
+
+    giveProduct(product)
+    {
+        this.setState({
+            name:product.Name
+            ,description:product.Description
+            ,price:product.Price
+            ,categoryId:product.CategoryId
+        });
     }
 
-    setCategories(categories){
-        this.setState({ categories:categories });
-
+    handleChange = e => this.setState({ [e.target.name]: e.target.value});
+    
+    
+    componentDidMount()
+    {
+        this.ListCategories();
+    }
+    componentDidUpdate()
+    {
+        if(this.props.onChange)
+        {
+            this.props.onChange(this.state);
+        }
     }
 
     ListCategories(){
-
-        axios.get('http://localhost/APISysLogistic/API/Category/List')
-        .then(response=>{
-            this.setState({ categories: response.data });
+        axios.get('http://'+this.state.ip+'/APISysLogistic/API/Category/List')
+        .then(response=>{            
+            this.setState({ categories: response.data.lstCategories });
         });
-
-        
+    }
+    
+    ClearProduct()
+    {
+        this.setState({
+            name:''
+            ,description:''
+            ,categoryId:0
+            ,price:0
+        });
     }
     SaveProduct()
     {
-        fetch('http://localhost/APISysLogistic/API/Product/Create?Name='+this.state.name+'&Price='+this.state.price+"&Description="+this.state.description+"&CategoryId="+this.state.categoryId)
+        axios.post('http://'+this.state.ip+'/APISysLogistic/API/Product/Create',null, {
+           params:{
+               'name':this.state.name,
+               'description':this.state.description,
+               'categoryId':this.state.categoryId,
+               'price':this.state.price
+           }
+        }).then(response=>{
+            var product = response.data;
+            this.props.findByProduct(product);
+        });
     }
     render(){
-        this.ListCategories();
+        
         return (
             <div>
                 <div className="row">
-                    <label>Name</label>
-                    <input type="text" id="txtName" name="txtName" value={this.state.name} onChange={this.handleChangeName} className="form-control"/>
-                </div>
-                <div className="row">
-                    <label>Description</label>
-                    <input type="text" id="txtDescription" name="txtDescription" onChange={this.handleChangeDescription} className="form-control"/>
-                </div>
-                <div className="row">
-                    <label>Price</label>
-                    <input type="text" id="txtPrice" name="txtPrice" value={this.state.price} onChange={this.handleChangePrice}   className="form-control"/>
-                </div>
-                <div className="row">
-                    <label>Category</label>
-                    <select id="cboCategory" onChange={this.handleChangeCategory} className="form-control">
-                    {this.state.categories &&
-                        this.state.categories.length > 0 &&
-                        this.state.categories.map(category => {
-                            return <option key={category.Id} value={category.Id}>{category.Name}</option>;
-                    })}
-                    
-                    
-                    </select>
-                </div>
-                <div>
-                    <button onClick={this.SaveProduct} className='btn btn-primary'>Registrar</button>
+                    <div className="col-md-6">
+                        <div className="row">
+                            <label>Name</label>
+                            <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange} className="form-control"/>
+                        </div>
+                        <div className="row">
+                            <label>Description</label>
+                            <input type="text" id="description" name="description" value= {this.state.description} onChange={this.handleChange} className="form-control"/>
+                        </div>
+                        <div className="row">
+                            <label>Price</label>
+                            <input type="text" id="price" name="price" value={this.state.price} onChange={this.handleChange}   className="form-control"/>
+                        </div>
+                        <div className="row">
+                            <label>Category</label>
+                            <select id="categoryId" name = "categoryId" value={this.state.categoryId} onChange={this.handleChange} className="form-control">
+                                <option value="0">Selection...</option>
+                                {
+                                    this.state.categories &&
+                                    this.state.categories.length > 0 && 
+                                    this.state.categories.map(category => {
+                                        return <option key={category.Id} value={category.Id}>{category.Name}</option>;
+                                    })
+                                }
+                            </select>
+                        </div>
+                        <div>
+                            <button onClick={this.SaveProduct} className='btn btn-primary'>Save</button>
+                        </div>
+                    </div>    
                 </div>
             </div>
         )
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,52 +1,75 @@
 import React, { Component } from 'react';
-import Product from './product'
+import axios from 'axios';
+import Product from './product';
+import { createRef } from 'react';
 class Products extends Component {
-    state = {
-        products:[]
+    constructor(props)
+    {
+        super(props);   
+        this.state = {
+            products:[],
+            ip:'192.168.0.25'
+            ,product:''
+        }
+        this.findByProduct = this.findByProduct.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.productElement = createRef();
     }
-
+    
+    findByProduct(product)
+    {
+        var list = this.state.products;
+        list.push(product);
+        this.setState({ products:list });
+    }
+    handleClick = objproduct =>
+    {
+        this.productElement.current.giveProduct(objproduct);
+    }
+    componentDidMount ()
+    {
+        this.ListProducts();
+    }
     ListProducts()
     {
-        //fetch('http://localhost/APISysLogistic/API/Product/List')
-        //.then((response)=>{
-        //    return response.json()
-        //})
-        //.then((products)=>{
-        //    this.setState({ products: products })
-        //})
-        //.catch(console.log)
+        axios.get('http://'+this.state.ip+'/APISysLogistic/API/Product/List')
+        .then(response=>{
+            this.setState({ products:response.data.lstRegisteredProducts })       
+        },(error)=>{
+            console.log(error);
+        });
     }
-    renderTableData()
-    {
-        return this.state.products.map((product,index) => {
-            const { id ,name, price } = product
-            return (
-                <tr key={id}>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{price}</td>
-                </tr>
-            )
-        })  
-    }
-    render(){
-        this.ListProducts();
+    
+    render(){        
         return(
             <div>
                 <div className="row">
                     <div className="col-md-6">
                         <h1 id="title">React Dynamic Table</h1>
                         <table id="products" className="table" >
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                    <th>Category</th>
+                                </tr>
+                            </thead>
                             <tbody>
-                                {this.renderTableData()}
+                                {this.state.products && 
+                                    this.state.products.length > 0 && 
+                                    this.state.products.map(product => {
+                                        return <tr key={product.Id} onClick={()=>this.handleClick(product)} ><td>{product.Id}</td><td>{product.Name}</td><td>{product.Description}</td><td>{product.Price}</td><td>{product.CategoryName}</td></tr>
+                                    })
+                                }
                             </tbody>
                         </table>
                     </div>
                     <div className="col-md-6">
-                        <Product />
-                    </div>
+                       <Product ref={this.productElement} onChange={this.onClick} findByProduct={this.findByProduct} />
+                    </div>                    
                 </div>
-                
             </div>
         )
     }
